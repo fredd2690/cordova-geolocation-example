ac5d8ac7e255be3ecd9f89d1121433235f5d92be
fredd2690 <fasdtheunique_26@hotmail.com>
2015-09-01 22:21:16 -0500

    add files
    agregando archivos

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\2015\someprojects\phonegap_p\applocalizacion\platforms\android\ant-build\AndroidManifest.xml : \
+D:\2015\someprojects\phonegap_p\applocalizacion\platforms\android\AndroidManifest.xml \
---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,16 @@
+D:\2015\someprojects\phonegap_p\applocalizacion\platforms\android\ant-build\classes.dex : \
+D:\2015\someprojects\phonegap_p\applocalizacion\platforms\android\ant-build\classes\fs\ej\localizacion\BuildConfig.class \
+D:\2015\someprojects\phonegap_p\applocalizacion\platforms\android\ant-build\classes\fs\ej\localizacion\CordovaApp.class \
+D:\2015\someprojects\phonegap_p\applocalizacion\platforms\android\ant-build\classes\fs\ej\localizacion\R$attr.class \
+D:\2015\someprojects\phonegap_p\applocalizacion\platforms\android\ant-build\classes\fs\ej\localizacion\R$drawable.class \
+D:\2015\someprojects\phonegap_p\applocalizacion\platforms\android\ant-build\classes\fs\ej\localizacion\R$string.class \
+D:\2015\someprojects\phonegap_p\applocalizacion\platforms\android\ant-build\classes\fs\ej\localizacion\R$xml.class \
+D:\2015\someprojects\phonegap_p\applocalizacion\platforms\android\ant-build\classes\fs\ej\localizacion\R.class \
+D:\2015\someprojects\phonegap_p\applocalizacion\platforms\android\ant-build\classes\io\cordova\hellocordova\BuildConfig.class \
+D:\2015\someprojects\phonegap_p\applocalizacion\platforms\android\ant-build\classes\io\cordova\hellocordova\CordovaApp.class \
+D:\2015\someprojects\phonegap_p\applocalizacion\platforms\android\ant-build\classes\io\cordova\hellocordova\R$attr.class \
+D:\2015\someprojects\phonegap_p\applocalizacion\platforms\android\ant-build\classes\io\cordova\hellocordova\R$drawable.class \
+D:\2015\someprojects\phonegap_p\applocalizacion\platforms\android\ant-build\classes\io\cordova\hellocordova\R$string.class \
+D:\2015\someprojects\phonegap_p\applocalizacion\platforms\android\ant-build\classes\io\cordova\hellocordova\R$xml.class \
+D:\2015\someprojects\phonegap_p\applocalizacion\platforms\android\ant-build\classes\io\cordova\hellocordova\R.class \
+D:\2015\someprojects\phonegap_p\applocalizacion\platforms\android\ant-build\dexedLibs\classes-9ddc6e83c2ac1a335a25667742079963.jar \
